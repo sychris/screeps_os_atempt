3bc6ba4a7ec131af32aba6c90ef4564751954aba
sychris <sychris9@gmail.com>
2024-05-27 17:33:34 -0400

    added builders to basic code

--- basic starter.js
+++ basic starter.js
@@ -8,6 +8,7 @@ function spawner() {
   
   let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
   let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
+  let builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
   
   if (harvesters.length < 2) {
     let newName = 'Harvester' + Game.time;
@@ -21,6 +22,12 @@ function spawner() {
     console.log('Spawning new upgrader: ' + newName);
     Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
       {memory: {role: 'upgrader'}});
+  }
+  if (builders.length < 1) {
+    let newName = 'builder' + Game.time;
+    console.log('Spawning new builder: ' + newName);
+    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+      {memory: {role: 'builder'}});
   }
   
   
@@ -40,6 +47,9 @@ function spawner() {
     }
     if (creep.memory.role === 'upgrader') {
       upgrader(creep);
+    }
+    if (creep.memory.role === 'builder') {
+      builder(creep);
     }
   }
 }
@@ -72,7 +82,7 @@ function harvester(creep) {
           creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
         }
         //move them close to Spawn if there's not a target to fill, so  they don't camp the resource node
-      }else{
+      } else {
         let targets = creep.room.find(FIND_STRUCTURES, {
           filter: (structure) => {
             return (structure.structureType === STRUCTURE_SPAWN)
@@ -106,5 +116,27 @@ function upgrader(creep) {
   }
 }
 
+function builder(creep) {
+  if (creep.memory.building === true) {
+    if (creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.building = false;
+      creep.say('🔄 harvest');
+    }
+    
+    let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) creep.moveTo(targets[0]);
+  }
+  if (creep.memory.building === false) {
+    if (creep.store.getFreeCapacity() === 0) {
+      creep.memory.building = true;
+      creep.say('⚡ building');
+    }
+    
+    let sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+    }
+  }
+}
 
 module.exports = spawner;
